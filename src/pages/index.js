4a4d57765bb0b32be0e909186e2004aaaa8c2097
Bastian PEIRE <54482913+BastianPEIRE@users.noreply.github.com>
2025-04-18 14:20:32 +0000

    fix header

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title" style={{ color: "white"}}>
+      <div className="container" style={{display: "flex", flexDirection: "column", gap: "2rem"}}>
+        <Heading as="h1" className="hero__title" style={{ color: "white", margin: "0"}}>
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle"  style={{ color: "white"}}>{siteConfig.tagline}</p>
