7a3f07a6e9abf4d684eb4ef1d7a5651079bfef6f
Bastian PEIRE <54482913+BastianPEIRE@users.noreply.github.com>
2025-05-06 08:09:34 +0000

    Deploy website - based on 1adf3a2bfefaebb287bb262314bfcb5f56de6389

---
+++ assets/js/a5e9e886.2d02b8ea.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[202],{2142:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/bp/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
